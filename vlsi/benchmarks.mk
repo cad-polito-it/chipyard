b5fdd86fe55d85eaea568012834f55acf30d6bd5
franout <francesco.angione@polito.it>
2026-06-04 16:43:18 +0200

    fix: changing tech_name to technology_name to avoid override from tutorial.mk

--- vlsi/benchmarks.mk
+++ vlsi/benchmarks.mk
@@ -2,7 +2,7 @@
 # makefile variables for VLSI benchmarks
 #########################################################################################
 benchmark ?= none
-tech_name ?= nangate45
+technology_name ?= nangate45
 toolchain ?= commercial
 
 EXTRA_CONFS ?=
@@ -10,13 +10,13 @@ EXTRA_CONFS ?=
 
 ifeq ($(benchmark),ibex)
     CONFIG            = IbexConfig
-    generated_src_name ?= generated-src-$(tech_name)
+    generated_src_name ?= generated-src-$(technology_name)
     HAMMER_EXEC       = ./example-vlsi
     TOOLS_CONF        ?= example-tools.yml
-    TECH_CONF         ?= ./technology/$(tech_name).yml
+    TECH_CONF         ?= ./technology/$(technology_name).yml
     FSIM_CONF_FILE    ?= ./fsim/example-fsim.yml
-    DESIGN_CONFS      ?= ./example-designs/$(tech_name)-$(toolchain).yml
-    VLSI_OBJ_DIR      ?= build-$(tech_name)-$(toolchain)-$(benchmark)
+    DESIGN_CONFS      ?= ./example-designs/$(technology_name)-$(toolchain).yml
+    VLSI_OBJ_DIR      ?= build-$(technology_name)-$(toolchain)-$(benchmark)
     INPUT_CONFS       ?= $(TOOLS_CONF) $(TECH_CONF) $(DESIGN_CONFS) $(EXTRA_CONFS)
 endif
 
@@ -24,38 +24,53 @@ endif
 
 ifeq ($(benchmark),rocket)
     CONFIG            = RocketConfig
-    generated_src_name ?= generated-src-$(tech_name)
+    generated_src_name ?= generated-src-$(technology_name)
     HAMMER_EXEC       = ./example-vlsi
     TOOLS_CONF        ?= example-tools.yml
-    TECH_CONF         ?= ./technology/$(tech_name).yml
+    TECH_CONF         ?= ./technology/$(technology_name).yml
     FSIM_CONF_FILE    ?= ./fsim/example-fsim.yml
-    DESIGN_CONFS      ?= ./example-designs/$(tech_name)-$(toolchain).yml
-    VLSI_OBJ_DIR      ?= build-$(tech_name)-$(toolchain)-$(benchmark)
+    DESIGN_CONFS      ?= ./example-designs/$(technology_name)-$(toolchain).yml
+    VLSI_OBJ_DIR      ?= build-$(technology_name)-$(toolchain)-$(benchmark)
     INPUT_CONFS       ?= $(TOOLS_CONF) $(TECH_CONF) $(DESIGN_CONFS) $(EXTRA_CONFS)
 endif
 
 
 ifeq ($(benchmark),boom-small)
     CONFIG            = SmallBoomV3Config
-    generated_src_name ?= generated-src-$(tech_name)
+    generated_src_name ?= generated-src-$(technology_name)
     HAMMER_EXEC       = ./example-vlsi
     TOOLS_CONF        ?= example-tools.yml
-    TECH_CONF         ?= ./technology/$(tech_name).yml
+    TECH_CONF         ?= ./technology/$(technology_name).yml
     FSIM_CONF_FILE    ?= ./fsim/example-fsim.yml
-    DESIGN_CONFS      ?= ./example-designs/$(tech_name)-$(toolchain).yml
-    VLSI_OBJ_DIR      ?= build-$(tech_name)-$(toolchain)-$(benchmark)
+    DESIGN_CONFS      ?= ./example-designs/$(technology_name)-$(toolchain).yml
+    VLSI_OBJ_DIR      ?= build-$(technology_name)-$(toolchain)-$(benchmark)
     INPUT_CONFS       ?= $(TOOLS_CONF) $(TECH_CONF) $(DESIGN_CONFS) $(EXTRA_CONFS)
 endif
 
 
 ifeq ($(benchmark),cva6)
     CONFIG            = CVA6Config
-    generated_src_name ?= generated-src-$(tech_name)
+    generated_src_name ?= generated-src-$(technology_name)
     HAMMER_EXEC       =  ./vlsi-cva6
     TOOLS_CONF        ?= example-tools.yml
-    TECH_CONF         ?= ./technology/$(tech_name).yml
+    TECH_CONF         ?= ./technology/$(technology_name).yml
     FSIM_CONF_FILE    ?= ./fsim/example-fsim.yml
-    DESIGN_CONFS      ?= ./example-designs/$(tech_name)-$(toolchain).yml
-    VLSI_OBJ_DIR      ?= build-$(tech_name)-$(toolchain)-$(benchmark)
+    DESIGN_CONFS      ?= ./example-designs/$(technology_name)-$(toolchain).yml
+    VLSI_OBJ_DIR      ?= build-$(technology_name)-$(toolchain)-$(benchmark)
     INPUT_CONFS       ?= $(TOOLS_CONF) $(TECH_CONF) $(DESIGN_CONFS) $(EXTRA_CONFS)
 endif
+
+
+ifeq ($(benchmark),radiance)
+    CONFIG            = RadianceTapeoutSimConfig 
+    generated_src_name ?= generated-src-$(technology_name)
+    HAMMER_EXEC       =  ./example-vlsi
+    TOOLS_CONF        ?= example-tools.yml
+    TECH_CONF         ?= ./technology/$(technology_name).yml
+    FSIM_CONF_FILE    ?= ./fsim/example-fsim.yml
+    DESIGN_CONFS      ?= ./example-designs/$(technology_name)-$(toolchain).yml
+    VLSI_OBJ_DIR      ?= build-$(technology_name)-$(toolchain)-$(benchmark)
+    INPUT_CONFS       ?= $(TOOLS_CONF) $(TECH_CONF) $(DESIGN_CONFS) $(EXTRA_CONFS)
+endif
+
+
